3065b755a42968faa325af3b034d7ed19ce35b25
NjeriNjoroge <grace.njoroge@incentro.com>
2019-07-15 12:53:36 +0300

    initial commit

---
+++ src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class Greeting {
+
+    //private final long id;
+    private final String content;
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    // public long getId() {
+    //     return id;
+    // }
+
+    public String getContent() {
+        return content;
+    }
+}
